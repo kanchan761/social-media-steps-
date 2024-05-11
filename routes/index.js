3893b0b5b03049da374ec8add7d96368bbcb8cf4
Your Name <you@example.comkanchanvishwakarma555555@gmail.com>
2024-05-11 10:33:17 +0530

    logout protected profile route

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,25 @@ failureRedirect :"/",
 
 );
 
+
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    next()
+  }
+  else{
+    res.redirect('/')
+  }
+}
+router.get("/logout" ,isLoggedIn, function (req,res,next){
+req.logout(()=>{
+  res.redirect('/')
+})
+
+
+})
+
+
 router.get('/Register', function(req, res, next) {
   res.render('register');
 });
@@ -30,20 +49,22 @@ router.post('/Register',async function(req, res, next) {
   // const newuser = new user(req.body)
   // await newuser.save()
   // res.redirect('/')
-
+  
   const {name , username , email , password} = req.body
   await user.register({name ,username ,email},password)
   res.redirect('/')
 }
 
 catch(error){
-res.json(error.message)
+  res.json(error.message)
 }
 });
 
-router.get("/profile", function(req,res,next){
-res.render("profile")
+router.get("/profile", isLoggedIn, function(req,res,next){
+  res.render("profile")
 })
+
+
 module.exports = router;
 
 
